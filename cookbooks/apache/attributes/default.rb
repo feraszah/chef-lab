3311c887e9f5e74e37b1d014acc74a42d601ddef
root <root@zahife1.mylabserver.com>
2015-08-05 02:09:49 +0000

    Updating my workstation chef-repo

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,6 +2,8 @@ default["apache"] ["sites"] ["zahife2"] = { "site_title" => "Feras's websites co
 default["apache"] ["sites"] ["zahife2b"] = { "site_title" => "Feras's secondary website coming soon", "port" => 80, "domain" => "zahife2b.mylabserver.com" }
 default["apache"] ["sites"] ["zahife3"] = { "site_title" => "Feras's 3rd website", "port" => 80, "domain" => "zahife3.mylabserver.com" }
 
+default["author"] ["name"] = "Feras"
+
 case node["platform"]
 when "centos"
 	default["apache"] ["package"] = "httpd"
---
+++ environments/dev.rb
@@ -0,0 +1,7 @@
+name "dev"
+description "This is the development environment"
+cookbook "apache", "= 0.1.5" 
+default_attributes({
+	"author" => {
+		"name" => "override worked!" }
+})
